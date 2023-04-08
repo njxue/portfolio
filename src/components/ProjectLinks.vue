f15ca827106ddcbd050bb3c1ac665b9d12ae0668
njxue <ngjxue@gmail.com>
2023-04-08 16:48:55 +0800

    Fix link

--- src/components/ProjectLinks.vue
+++ src/components/ProjectLinks.vue
@@ -17,7 +17,7 @@
       </svg>
     </a>
     <a :href="githubLink" class="w-8"
-      ><img src="src/assets/icons/github.svg" width="40"
+      ><img src="../assets/icons/github.svg" width="40"
     /></a>
   </div>
 </template>
