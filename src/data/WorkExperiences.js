24125fea4cada40243b5bb781f421f896245c5ce
njxue <ngjxue@gmail.com>
2024-05-30 21:33:03 +0800

    Update work experience at Aastar

--- src/data/WorkExperiences.js
+++ src/data/WorkExperiences.js
@@ -1,6 +1,6 @@
 const WorkExperiences = [
   {
-    name: "Aastar Trading Pte Ltd @ KPN Corp",
+    name: "Aastar Trading Pte Ltd | KPN Corp",
     role: "Software Developer Intern",
     startDate: "May 2023",
     endDate: "Nov 2023",
@@ -10,10 +10,11 @@ const WorkExperiences = [
       oleochemicals and green energy.
       `,
     deliverables: [
-      "Streamlined critical business procedures such as Accounting and Order Confirmation via automation, increasing operational efficiency",
-      "Revamped and enhanced existing codebase through extensive refactoring, resulting in remarkable performance boost of up to 200%",
-      "Enhanced the user interface and user experience of a corporate portal, benefiting over 2,000 employees",
-      "Initiated and led the development of detailed code documentation, facilitating the integration of future interns into the codebase"
+      "Automated business processes such as compiling and generating accounting reports, enhancing operational efficiency and minimizing human error",
+      "Streamlined critical business procedures by introducing Quality-of-Life features to the company's business portal in React and C# as well as ad-hoc bug fixes",
+      "Revamped and enhanced the existing codebase through extensive refactoring, resulting in improved performance and code maintainability",
+      "Successfully migrated a vital and complex business procedure from the legacy business portal to the new one, resulting in remarkable performance boosts and significantly reducing manual labor",
+      "Initiated and led the development of detailed code documentation, incorporating UML and sequence diagrams, to facilitate the integration of future developers into the codebase"
     ],
     techs: ["react", "csharp", "javascript", "mysql"],
     logo: "/images/kpnLogo.png",
