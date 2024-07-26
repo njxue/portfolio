32f3ca8e07b6d4951b062bb2f6d3d9a92de1a48f
njxue <ngjxue@gmail.com>
2024-07-26 10:59:40 +0800

    Add work experience at Nas

--- src/components/work/Experience.vue
+++ src/components/work/Experience.vue
@@ -2,7 +2,7 @@
   <div
     class="experience p-5 border-gray shadow-lg border rounded-lg invisible dark:bg-[#252525] dark:border-[#252525]">
     <div class="pb-5 text-2xl flex justify-start items-center gap-3">
-      <img class="w-20" :src="experience.logo" />
+      <img class="w-20 rounded-full" :src="experience.logo" />
       <div>
         <p>{{ experience.role }}</p>
         <p class="text-sm text-blue-900 dark:text-red-600">
@@ -14,7 +14,7 @@
       </div>
     </div>
     <hr class="dark:border-[#3d3d3d]" />
-    <div class="my-3">{{ experience.companyDescription }}</div>
+    <!-- <div class="my-3">{{ experience.companyDescription }}</div> -->
     <ul class="list-disc list-outside pl-4">
       <li class="my-2" v-for="deliverable in experience.deliverables">
         {{ deliverable }}
--- src/data/WorkExperiences.js
+++ src/data/WorkExperiences.js
@@ -1,11 +1,22 @@
 const WorkExperiences = [
   {
+    name: "Nas Company",
+    role: "Software Engineer Intern",
+    startDate: "Jun 2024",
+    endDate: "Present",
+    deliverables: [
+      "Implemented core frontend features for Nas.io, a community building platform, using Next.js to significantly enhance user experience.",
+      "Integrated PostHog with Next.js to systematically collect and analyze web performance metrics, including network performance and core web vitals, delivering key insights for improving SEO.",
+    ],
+    techs: ["next", "tailwind", "posthog"],
+    logo: "/images/nasLogo.jfif",
+  },
+  {
     name: "Aastar Trading Pte Ltd | KPN Corp",
     role: "Software Developer Intern",
     startDate: "May 2023",
     endDate: "Nov 2023",
-    companyDescription:
-      `
+    companyDescription: `
       Aastar Trading is an international trading company based in Singapore, specialised in procurement and distribution of cement products, fertilizer, oils & fats, consumer products, 
       oleochemicals and green energy.
       `,
@@ -14,7 +25,7 @@ const WorkExperiences = [
       "Streamlined critical business procedures by introducing Quality-of-Life features to the company's business portal in React and C# as well as ad-hoc bug fixes",
       "Revamped and enhanced the existing codebase through extensive refactoring, resulting in improved performance and code maintainability",
       "Successfully migrated a vital and complex business procedure from the legacy business portal to the new one, resulting in remarkable performance boosts and significantly reducing manual labor",
-      "Initiated and led the development of detailed code documentation, incorporating UML and sequence diagrams, to facilitate the integration of future developers into the codebase"
+      "Initiated and led the development of detailed code documentation, incorporating UML and sequence diagrams, to facilitate the integration of future developers into the codebase",
     ],
     techs: ["react", "csharp", "javascript", "mysql"],
     logo: "/images/kpnLogo.png",
